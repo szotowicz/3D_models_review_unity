5fce088995c92506a8aa401c8c377bfe3149dbeb
mszotowicz <mikolaj.szotowicz@gmail.com>
2019-01-17 17:30:30 +0100

    Add UI

---
+++ Assets/Scripts/FlyCamera.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class FlyCamera : MonoBehaviour
+{
+    private readonly float mainSpeed = 0.5f;
+    private readonly float camSensitive = 0.25f;
+    private Vector3 lastMouse = new Vector3(255.0f, 255.0f, 155.0f);
+
+    void Update()
+    {
+        if (!Input.GetKey(KeyCode.Q))
+        {
+            lastMouse = Input.mousePosition - lastMouse;
+            lastMouse = new Vector3(-lastMouse.y * camSensitive, lastMouse.x * camSensitive, 0);
+            lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+            transform.eulerAngles = lastMouse;
+            lastMouse = Input.mousePosition;
+
+            Vector3 possition = GetBaseInput();
+            possition *= mainSpeed;
+
+            Vector3 newPosition = transform.position;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.Translate(possition);
+            }
+            else
+            {
+                transform.Translate(possition);
+                newPosition.x = transform.position.x;
+                newPosition.z = transform.position.z;
+                transform.position = newPosition;
+            }
+        }
+    }
+
+    private Vector3 GetBaseInput()
+    {
+        Vector3 direction = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += new Vector3(1, 0, 0);
+        }
+        return direction;
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public GameObject Model1;
+    public GameObject Model2;
+    public GameObject Model3;
+
+    public void SelectModel1()
+    {
+        Debug.Log("SelectModel1");
+    }
+
+    public void SelectModel2()
+    {
+        Debug.Log("SelectModel2");
+    }
+
+    public void SelectModel3()
+    {
+        Debug.Log("SelectModel3");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
